24e6f9db7096198b9be5e9eb9a667ffa36866584
Oscar Eduardo <oscar.eduardo.raygoza@gmail.com>
2019-11-01 00:50:11 -0600

    include function StringOnJson

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,7 +7,41 @@ ctrl.index = async (req, res) => {
     return res;
   });
 
-  res.render("index", { title: "example - cat finder", message: "example" });
+  function JsonOnSring(dataJson){
+    let objJson ='{ "Sensores" : [ {';
+    let pivote=0, i= -1;
+    let value;
+    let props= 1;
+    let flag = false;
+    while(pivote >= 0){
+        
+        pivote = dataJson.indexOf(",",i+1);
+        if(pivote == -1){
+            flag = true;
+            value = dataJson.substring(i+1, dataJson.length);
+        }
+            
+        if(!flag)
+        value = dataJson.substring(i+1, pivote);
+    
+      
+    
+        objJson += `
+           "${props}": "${value}",
+        `;
+        
+        i = pivote;
+        props++;
+        
+        if(flag)
+        break;
+    }
+    
+    objJson += '"FILTER": "ON" } ]}';
+    return objJson;
+    }
+    
+  res.render("index", { title: "example - cat finder", message: "example", data: JsonOnSring(data) });
 };
 
 module.exports = ctrl;
