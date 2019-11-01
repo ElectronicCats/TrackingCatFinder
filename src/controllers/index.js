be21cb95a223b287479489efcddf84b43c30064b
Oscar Eduardo <oscar.eduardo.raygoza@gmail.com>
2019-11-01 00:28:00 -0600

    config pug view engine

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,13 @@
+const axios = require("axios");
+
 const ctrl = {};
 
-ctrl.index = (req, res) => {
-  res.render("index");
+ctrl.index = async (req, res) => {
+  let data = axios.get("http://192.168.1.4:80/data", res => {
+    return res;
+  });
+
+  res.render("index", { title: "example - cat finder", message: "example" });
 };
 
 module.exports = ctrl;
--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const errorhandler = require("errorhandler");
 
@@ -8,12 +10,14 @@ module.exports = app => {
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+
+  app.set("views", path.join(__dirname, "../views"));
+  app.set("view engine", "pug"); //View engine
   // routes
   routes(app);
 
   // static files
-  //app.use("/public", express.static(path.join(__dirname, "../public")));
-
+  app.use("/public", express.static(path.join(__dirname, "../public")));
   //errorhandlers
   if ("development" === app.get("env")) {
     app.use(errorhandler);
