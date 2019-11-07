9691d23191cff831f9bfebe5c0074045a5360580
Oscar Eduardo <oscar.eduardo.raygoza@gmail.com>
2019-11-07 13:16:15 -0600

    fix send data esp32

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,47 +3,53 @@ const axios = require("axios");
 const ctrl = {};
 
 ctrl.index = async (req, res) => {
-  let data = axios.get("http://192.168.1.4:80/data", res => {
-    return res;
-  });
+  let data = axios
+    .get("http://192.168.4.1/data")
+    .then(result => {
+      console.log(result.data);
+      res.render("index", {
+        title: "example - cat finder",
+        message: "Tracking Cat Finder",
+        data: result.data
+      });
+    })
+    .catch(err => {
+      res.render("index", {
+        title: "example - cat finder",
+        message: "Tracking Cat Finder",
+        data: err.message
+      });
+    });
 
-  function JsonOnSring(dataJson){
-    let objJson ='{ "Sensores" : [ {';
-    let pivote=0, i= -1;
+  function JsonOnSring(dataJson) {
+    let objJson = '{ "Sensores" : [ {';
+    let pivote = 0,
+      i = -1;
     let value;
-    let props= 1;
+    let props = 1;
     let flag = false;
-    while(pivote >= 0){
-        
-        pivote = dataJson.indexOf(",",i+1);
-        if(pivote == -1){
-            flag = true;
-            value = dataJson.substring(i+1, dataJson.length);
-        }
-            
-        if(!flag)
-        value = dataJson.substring(i+1, pivote);
-    
-      
-    
-        objJson += `
+    while (pivote >= 0) {
+      pivote = dataJson.indexOf(",", i + 1);
+      if (pivote == -1) {
+        flag = true;
+        value = dataJson.substring(i + 1, dataJson.length);
+      }
+
+      if (!flag) value = dataJson.substring(i + 1, pivote);
+
+      objJson += `
            "${props}": "${value}",
         `;
-        
-        i = pivote;
-        props++;
-        
-        if(flag)
-        break;
+
+      i = pivote;
+      props++;
+
+      if (flag) break;
     }
-    
+
     objJson += '"FILTER": "ON" } ]}';
     return objJson;
-    }
-    let parseData = {
-      data: JsonOnSring(data)
-    };
-  res.render("index", { title: "example - cat finder", message: "example", data: parseData });
+  }
 };
 
 module.exports = ctrl;
